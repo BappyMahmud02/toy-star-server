4d31cc8ff947cbeb1b89f350c26bf0b2ae45b339
Bappy <bappymahmud321@gmail.com>
2023-05-18 19:46:54 +0600

    added mongo server database

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 const express = require('express');
- const cors = require('cors');
- const { MongoClient, ServerApiVersion } = require('mongodb');
- require('dotenv').config()
- const app = express();
- const port = process.env.PORT || 5000 ;
+const cors = require('cors');
+const { MongoClient, ServerApiVersion } = require('mongodb');
+require('dotenv').config()
+const app = express();
+const port = process.env.PORT || 5000;
 
 
 //  middleware
@@ -14,26 +14,43 @@ console.log(process.env.DB_PASS);
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.qagbjwf.mongodb.net/?retryWrites=true&w=majority`;
 
+
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
-  serverApi: {
-    version: ServerApiVersion.v1,
-    strict: true,
-    deprecationErrors: true,
-  }
+    serverApi: {
+        version: ServerApiVersion.v1,
+        strict: true,
+        deprecationErrors: true,
+    }
 });
 
 async function run() {
-  try {
-    // Connect the client to the server	(optional starting in v4.7)
-    await client.connect();
-    // Send a ping to confirm a successful connection
-    await client.db("admin").command({ ping: 1 });
-    console.log("Pinged your deployment. You successfully connected to MongoDB!");
-  } finally {
-    // Ensures that the client will close when you finish/error
-    await client.close();
-  }
+    try {
+        // Connect the client to the server	(optional starting in v4.7)
+        const toysCollection = client.db('toys').collection('toysCollection')
+
+
+        app.post('/alltoys', async (req, res) => {
+            const toys = req.body
+            const result = await toysCollection.insertOne(toys)
+            res.send(result);
+        })
+        app.get("/toys", async (req, res) => {
+            const cursor = toysCollection.find();
+            const result = await cursor.toArray();
+            res.send(result);
+        });
+
+        // await client.connect();
+
+
+        // Send a ping to confirm a successful connection
+        await client.db("admin").command({ ping: 1 });
+        console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    } finally {
+        // Ensures that the client will close when you finish/error
+        // await client.close();
+    }
 }
 run().catch(console.dir);
 
