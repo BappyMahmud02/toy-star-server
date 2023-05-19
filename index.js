dc6995b45c3e312d7ebb5da16f899d1294c3f3e5
Bappy <bappymahmud321@gmail.com>
2023-05-19 11:59:38 +0600

    get single ids

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const cors = require('cors');
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 require('dotenv').config()
 const app = express();
 const port = process.env.PORT || 5000;
@@ -30,15 +30,24 @@ async function run() {
         const toysCollection = client.db('toys').collection('toysCollection')
 
 
+        
+
         app.post('/alltoys', async (req, res) => {
             const toys = req.body
             const result = await toysCollection.insertOne(toys)
             res.send(result);
         })
+
         app.get("/toys", async (req, res) => {
             const cursor = toysCollection.find();
             const result = await cursor.toArray();
             res.send(result);
+        });
+        app.get("/toys/:id", async (req, res) => {
+            const id = req.params.id
+            const query = {_id: new ObjectId(id)}
+            const singleToys = await toysCollection.findOne(query)
+            res.send(singleToys)
         });
 
         // await client.connect();
